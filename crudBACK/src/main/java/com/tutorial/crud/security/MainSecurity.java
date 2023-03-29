66ebc32736e98db0d0c155434308a335a8969caa
RaulCudris <raulcudriz@hotmail.com>
2023-03-29 16:38:51 -0500

    Actualizacion del Proyecto

---
+++ crudBACK/src/main/java/com/tutorial/crud/security/MainSecurity.java
@@ -0,0 +1,8 @@
+package com.tutorial.crud.security;
+
+public class MainSecurity {
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
